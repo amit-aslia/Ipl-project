41cb4b4838fce9834132f14f51e5940250b90c45
amit <amit.1@mountblue.io>
2019-09-06 18:14:43 +0100

    Second FUnction visualization Done

--- index.js
+++ index.js
@@ -33,8 +33,48 @@ function matchesPerYear()  // Function matchesPerYear
    return matchesPerYears;
 }
 // ********************Second Function*******************************
+function matchesWonPerYear()
+{
+    let years=matches.reduce((acc,year)=>
+    {
+       acc[year.season]=(acc[year.season] || 0); 
+       return acc;
+    },{})
+    
+    let team=matches.reduce((acc,Team)=>
+    {
+        acc[Team.winner]=(acc[Team.winner] || 0)
+        return acc;
+    },[])
+    delete team[''];
+    var teams = Object.keys(team).map(function(key) {
+        return key;
+      });
+      
+    
+    //console.log(teams)
+    var matchesWonPerYear=matches.filter((checkWinner)=>
+    {
+        if(checkWinner.winner!=' ')
+        {
+            return checkWinner;
+        }
+    }).reduce((acc,match)=>
+    {
+        acc[match.winner]=(acc[match.winner] || Object.assign({},years));
+        acc[match.winner][match.season]=(acc[match.winner][match.season] || 0)+1;
+        return acc;
+    },{})
+    delete matchesWonPerYear['']; 
+    //console.log(matchesWonPerYear)
+    let teamStat={"Team":teams,"valuePerYear":Object.values(matchesWonPerYear)};
+   // console.log(teamStat)
+    return teamStat;
 
+    //console.log(matchesWonPerYear)
 
+    
+}
 
 
 
@@ -43,7 +83,7 @@ function matchesPerYear()  // Function matchesPerYear
 
 //Function Calling
 matchesPerYear()
-// let teamStat= (matchesWonPerYear());
+let teamStat= (matchesWonPerYear());
 // let extraRunStat =extraRunConceded();
 
 
@@ -67,8 +107,9 @@ matchesPerYear()
 
 
 // ***********************************************************************************
-let data={matchesPerYears};
+//let data={matchesPerYears};
 // let data={matchesPerYears,teamStat,extraRunStat};
+ let data={matchesPerYears,teamStat};
 //let data={matchesPerYears};
 
 (async () => {
