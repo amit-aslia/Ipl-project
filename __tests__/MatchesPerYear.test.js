efe7689445efa20ca007b356e0603dc96ca19a92
amit <amit.1@mountblue.io>
2019-09-07 10:46:24 +0100

    All files are updated in master

---
+++ __tests__/MatchesPerYear.test.js
@@ -0,0 +1,28 @@
+let matchesPerYear = require("../ipl/matchesPerYear");
+
+describe("Matches Played per season", () => {
+  it("Matches Played in Every Season", () => {
+    const matches= [
+      {season:2013},
+      {season:2013},
+      {season:2013},
+      {season:2009},
+      {season:2009},
+      {season:2011},
+      {season:2011},
+      {season:2011},	 
+    ];
+    const expectedOutput = {
+      "year":["2009","2011","2013"],
+      "Matches":[2,3,3]
+    };
+    expect(matchesPerYear(matches)).toEqual(expectedOutput);
+  });
+  it("Matches Played if there is no data in dataset", () => {
+    const dataSet = [];
+    const expectedOutput = {"Matches":[],
+                             "year":[] };
+    expect(matchesPerYear(dataSet)).toEqual(expectedOutput);
+  });
+})
+    // return Matchesperyear;
---
+++ ipl/matchesPerYear.js
@@ -0,0 +1,16 @@
+function matchesPerYear(dataSet){
+    var matchesPerYear= dataSet
+    .reduce((acc,year) => {
+         if(acc[year.season]){
+             acc[year.season]++;
+         }
+         else{
+             acc[year.season]=1;
+         }
+         return acc;
+     },{});            
+     return matchesPerYear;
+    
+ }
+
+module.exports =  matchesPerYear ;
